fcb4bacbd64810515d791a6df39b9006a0934e96
Brown, Davis R <davis.brown@pnnl.gov>
2022-04-30 20:34:22 -0700

    sigmoid and gating try 1

--- zca_gating_expts/cifar_model.py
+++ zca_gating_expts/cifar_model.py
@@ -41,15 +41,24 @@ class ConvSigmoid(nn.Module):
     Projection implemented via a 1x1 convolution."""
 
     def __init__(
-        self,
-        in_channels, out_channels, kernel_size, bias=False, padding=1,
-        mode=0
+        self, in_channels, out_channels, kernel_size, bias=False, padding=1, mode=0
     ):
         super(ConvSigmoid, self).__init__()
-        self.conv_weight = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias).cuda()
+        self.conv_weight = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=padding,
+            bias=bias,
+        ).cuda()
 
-
-        self.conv_gating = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias).cuda()
+        self.conv_gating = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=padding,
+            bias=bias,
+        ).cuda()
 
         self.conv_gating.weight.data = self.conv_weight.weight.data.clone()
 
@@ -62,9 +71,8 @@ class ConvSigmoid(nn.Module):
             for param in self.conv_gating.parameters():
                 param.requires_grad = False
 
-
     def forward(self, x, x_init=None):
-        x = torch.sigmoid(self.conv_gating(x))*self.conv_weight(x)
+        x = torch.sigmoid(self.conv_gating(x)) * self.conv_weight(x)
         return x
 
 
@@ -75,14 +83,32 @@ class ConvSigmoidNorm(nn.Module):
 
     def __init__(
         self,
-        in_channels, out_channels, kernel_size, bias=False, padding=1,
-        mode=0
+        in_channels,
+        out_channels,
+        kernel_size,
+        bias=False,
+        padding=1,
+        stride=1,
+        mode=0,
     ):
         super(ConvSigmoidNorm, self).__init__()
-        self.conv_weight = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias).cuda()
+        self.conv_weight = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+        ).cuda()
 
-
-        self.conv_gating = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias).cuda()
+        self.conv_gating = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+        ).cuda()
 
         self.conv_gating.weight.data = self.conv_weight.weight.data.clone()
 
@@ -95,9 +121,62 @@ class ConvSigmoidNorm(nn.Module):
             for param in self.conv_gating.parameters():
                 param.requires_grad = False
 
-
     def forward(self, x, norm_layer, x_init=None):
-        x = torch.sigmoid(self.conv_gating(x))*norm_layer(self.conv_weight(x))
+        x = torch.sigmoid(self.conv_gating(x)) * norm_layer(self.conv_weight(x))
+        return x
+
+
+class ConvSigmoidNormResidual(nn.Module):
+    """Modifies an nn.Module from W to MW,
+    where W is the original op and M is an orthonormal matrix.
+    Projection implemented via a 1x1 convolution."""
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        bias=False,
+        padding=1,
+        stride=1,
+        mode=0,
+    ):
+        super(ConvSigmoidNormResidual, self).__init__()
+        self.conv_weight = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+        ).cuda()
+
+        self.conv_gating = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+        ).cuda()
+
+        self.conv_gating.weight.data = self.conv_weight.weight.data.clone()
+
+        self.w_init = self.conv_weight.weight.data.clone()
+
+        if mode == 1:
+            for param in self.conv_weight.parameters():
+                param.requires_grad = False
+        if mode == 2:
+            for param in self.conv_gating.parameters():
+                param.requires_grad = False
+
+    def forward(self, x, norm_layer, identity, downsample, x_init=None):
+        if downsample is not None:
+            identity = downsample(identity)
+        x = norm_layer(self.conv_weight(x))
+        x += identity
+        x = torch.sigmoid(self.conv_gating(x)) * x
         return x
 
 
@@ -107,15 +186,24 @@ class ConvTwoStream(nn.Module):
     Projection implemented via a 1x1 convolution."""
 
     def __init__(
-        self,
-        in_channels, out_channels, kernel_size, bias=False, padding=1,
-        mode=0
+        self, in_channels, out_channels, kernel_size, bias=False, padding=1, mode=0
     ):
         super(ConvTwoStream, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias).cuda()
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=padding,
+            bias=bias,
+        ).cuda()
 
-
-        self.conv_init = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias).cuda()
+        self.conv_init = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=padding,
+            bias=bias,
+        ).cuda()
 
         if mode != 2:
             self.conv_init.weight.data = self.conv.weight.data.clone()
@@ -129,7 +217,6 @@ class ConvTwoStream(nn.Module):
 
         self.mode = mode
 
-
     def forward(self, x, x_init=None):
         if x_init is None:
             x_init = x.clone()
@@ -140,7 +227,7 @@ class ConvTwoStream(nn.Module):
 
         if self.mode == 1 or self.mode == 2 or self.mode == 3:
             x_init = self.conv_init(x_init)
-            x = self.conv(x)*(x_init > 0).detach()
+            x = self.conv(x) * (x_init > 0).detach()
             x_init = F.relu(x_init)
 
         return x, x_init
@@ -153,13 +240,32 @@ class ConvTwoStreamNorm(nn.Module):
 
     def __init__(
         self,
-        in_channels, out_channels, kernel_size, stride=1, bias=False, padding=1, mode=0
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        bias=False,
+        padding=1,
+        mode=0,
     ):
         super(ConvTwoStreamNorm, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias).cuda()
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+        ).cuda()
 
-
-        self.conv_init = nn.Conv2d(in_channels, out_channels, stride=stride, kernel_size=kernel_size, padding=padding, bias=bias).cuda()
+        self.conv_init = nn.Conv2d(
+            in_channels,
+            out_channels,
+            stride=stride,
+            kernel_size=kernel_size,
+            padding=padding,
+            bias=bias,
+        ).cuda()
 
         if mode != 2:
             self.conv_init.weight.data = self.conv.weight.data.clone()
@@ -173,7 +279,6 @@ class ConvTwoStreamNorm(nn.Module):
 
         self.mode = mode
 
-
     def forward(self, x, norm_layer, x_init=None):
         if x_init is None:
             x_init = x.clone()
@@ -184,7 +289,7 @@ class ConvTwoStreamNorm(nn.Module):
 
         if self.mode == 1 or self.mode == 2 or self.mode == 3:
             x_init = self.conv_init(x_init)
-            x = norm_layer(self.conv(x))*(x_init > 0).detach()
+            x = norm_layer(self.conv(x)) * (x_init > 0).detach()
             x_init = F.relu(x_init)
 
         return x, x_init
@@ -197,14 +302,32 @@ class ConvTwoStreamResidual(nn.Module):
 
     def __init__(
         self,
-        in_channels, out_channels, kernel_size, stride=1, bias=False, padding=1,
-        mode=0
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        bias=False,
+        padding=1,
+        mode=0,
     ):
         super(ConvTwoStreamResidual, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias).cuda()
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+        ).cuda()
 
-
-        self.conv_init = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias).cuda()
+        self.conv_init = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+        ).cuda()
 
         if mode != 2:
             self.conv_init.weight.data = self.conv.weight.data.clone()
@@ -217,7 +340,6 @@ class ConvTwoStreamResidual(nn.Module):
                 param.requires_grad = False
 
         self.mode = mode
-
 
     def forward(self, x, norm_layer, identity, downsample, x_init=None):
         if downsample is not None:
@@ -237,11 +359,10 @@ class ConvTwoStreamResidual(nn.Module):
             x = norm_layer(self.conv(x))
 
             x += identity
-            x = x*(x_init > 0).detach()
+            x = x * (x_init > 0).detach()
             x_init = F.relu(x_init)
 
         return x, x_init
-
 
 
 def mCNN_k_g(c=64, num_classes=10, mode=0):  # no Batch Norm
@@ -263,37 +384,38 @@ def mCNN_k_g(c=64, num_classes=10, mode=0):  # no Batch Norm
     )
 
 
-
-
 class mCNN_fixed(nn.Module):
     """Modifies an nn.Module from W to MW,
     where W is the original op and M is an orthonormal matrix.
     Projection implemented via a 1x1 convolution."""
 
-    def __init__(
-        self,
-        c=64, num_classes=10, mode=0
-    ):
+    def __init__(self, c=64, num_classes=10, mode=0):
         super(mCNN_fixed, self).__init__()
 
         self.mode = mode
 
-        self.conv1 = ConvTwoStream(3, c, kernel_size=3, padding=1, bias=True, mode = mode).cuda()
+        self.conv1 = ConvTwoStream(
+            3, c, kernel_size=3, padding=1, bias=True, mode=mode
+        ).cuda()
 
-        self.conv2 = ConvTwoStream(c, c * 2, kernel_size=3, padding=1, bias=False, mode=mode).cuda()
+        self.conv2 = ConvTwoStream(
+            c, c * 2, kernel_size=3, padding=1, bias=False, mode=mode
+        ).cuda()
 
         self.mp_2 = nn.MaxPool2d(2)
         self.mp_4 = nn.MaxPool2d(4)
 
-        self.conv3 = ConvTwoStream(c * 2, c * 4, kernel_size=3, padding=1, bias=False, mode=mode).cuda()
+        self.conv3 = ConvTwoStream(
+            c * 2, c * 4, kernel_size=3, padding=1, bias=False, mode=mode
+        ).cuda()
 
-        self.conv4 = ConvTwoStream(c * 4, c * 8, kernel_size=3, padding=1, bias=False, mode=mode).cuda()
+        self.conv4 = ConvTwoStream(
+            c * 4, c * 8, kernel_size=3, padding=1, bias=False, mode=mode
+        ).cuda()
 
         self.flatten = nn.Flatten()
 
         self.linear = nn.Linear(c * 8, num_classes, bias=False)
-
-
 
     def forward(self, x):
         x, x_init = self.conv1(x)
@@ -306,7 +428,7 @@ class mCNN_fixed(nn.Module):
         x, x_init = self.conv4(x, x_init)
         x = self.mp_2(x)
         # x_init = self.mp_2(x_init)
-        x = self.mp_4(x) # x_init = self.mp_4(x_init)
+        x = self.mp_4(x)  # x_init = self.mp_4(x_init)
         # x_init = self.flatten(x_init)
         x = self.flatten(x)
 
@@ -342,6 +464,7 @@ def mCNN_bn_k(c=64, num_classes=10):
         Flatten(),
         nn.Linear(c * 8, num_classes, bias=True),
     )
+
 
 def mCNN(c=64, num_classes=10):
     return mCNN_bn_k(c, num_classes)
--- zca_gating_expts/gating_resnet.py
+++ zca_gating_expts/gating_resnet.py
@@ -1,20 +1,12 @@
 import torch
 import torch.nn as nn
-from cifar_model import ConvTwoStream, ConvTwoStreamResidual, ConvTwoStreamNorm
-
-
-def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(
-        in_planes,
-        out_planes,
-        kernel_size=3,
-        stride=stride,
-        padding=dilation,
-        groups=groups,
-        bias=False,
-        dilation=dilation,
-    )
+from cifar_model import (
+    ConvTwoStream,
+    ConvTwoStreamResidual,
+    ConvTwoStreamNorm,
+    ConvSigmoidNorm,
+    ConvSigmoidNormResidual,
+)
 
 
 def conv1x1(in_planes, out_planes, stride=1):
@@ -45,14 +37,11 @@ class BasicBlockFixed(nn.Module):
         if dilation > 1:
             raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        # self.conv1 = conv3x3(inplanes, planes, stride)
         self.conv1_two_stream = ConvTwoStreamNorm(
             inplanes, planes, kernel_size=3, stride=stride, mode=mode
         )
 
         self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        # self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
@@ -67,7 +56,6 @@ class BasicBlockFixed(nn.Module):
 
         # out = self.conv1(x)
         # out = self.bn1(out)
-        # out = self.relu(out)
         out, x_init = self.conv1_two_stream(x, self.bn1, x_init)
 
         # out = self.conv2(out)
@@ -77,7 +65,6 @@ class BasicBlockFixed(nn.Module):
         #     identity = self.downsample(x)
 
         # out += identity
-        # out = self.relu(out)
         out, x_init = self.conv2_two_stream(
             out, self.bn2, identity, downsample=self.downsample, x_init=x_init
         )
@@ -98,6 +85,7 @@ class BasicBlockSigmoidGating(nn.Module):
         base_width=64,
         dilation=1,
         norm_layer=None,
+        mode=0,
     ):
         super(BasicBlockSigmoidGating, self).__init__()
         if norm_layer is None:
@@ -106,37 +94,27 @@ class BasicBlockSigmoidGating(nn.Module):
             raise ValueError("BasicBlock only supports groups=1 and base_width=64")
         if dilation > 1:
             raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        # self.conv1 = conv3x3(inplanes, planes, stride)
-        self.conv1_two_stream = ConvTwoStreamNorm(
-            inplanes, planes, kernel_size=1, stride=stride
+        self.conv1_sigmoid = ConvSigmoidNorm(
+            inplanes, planes, kernel_size=3, stride=stride, mode=mode
         )
 
         self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        # self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
-        self.conv2_two_stream = ConvTwoStreamResidual(planes, planes, kernel_size=3)
+        self.conv2_two_stream = ConvSigmoidNormResidual(
+            planes, planes, kernel_size=3, mode=mode, stride=1
+        )
 
-    def forward(self, x, x_init):
+    # def forward(self, x, x_init):
+    def forward(self, x):
         identity = x
-
-        # out = self.conv1(x)
-        # out = self.bn1(out)
-        # out = self.relu(out)
-        out = self.conv1_two_stream(x, self.bn1, x_init)
-
-        # out = self.conv2(out)
-        # out = self.bn2(out)
-
-        # if self.downsample is not None:
-        #     identity = self.downsample(x)
-
-        # out += identity
-        # out = self.relu(out)
-        out = self.conv2_two_stream(out, self.bn2, identity, self.downsample, x_init)
+        out = self.conv1_sigmoid(x, self.bn1)
+        out = self.conv2_two_stream(
+            out, self.bn2, identity, downsample=self.downsample
+        )
 
         return out
 
@@ -146,7 +124,7 @@ class ResNetFixed(nn.Module):
         self,
         block,
         layers,
-        num_classes=1000,
+        num_classes=100,
         zero_init_residual=False,
         groups=1,
         width_per_group=64,
@@ -182,7 +160,6 @@ class ResNetFixed(nn.Module):
         #     3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False
         # )
         self.bn1 = norm_layer(self.inplanes)
-        # self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
         self.layer1 = self._make_layer(block, 64, layers[0])
@@ -273,7 +250,6 @@ class ResNetFixed(nn.Module):
         x, x_init = self.conv1_two_stream(x, self.bn1)
         # x = self.conv1(x)
         # x = self.bn1(x)
-        # x = self.relu(x)
         x = self.maxpool(x)
         x_init = self.maxpool(x_init)
 
@@ -292,8 +268,158 @@ class ResNetFixed(nn.Module):
         return self._forward_impl(x)
 
 
-def _resnet(arch, block, layers, mode, **kwargs):
-    # model = ResNet(block, layers, mode=mode, **kwargs)
+class ResNetSigmoid(nn.Module):
+    def __init__(
+        self,
+        block,
+        layers,
+        num_classes=100,
+        zero_init_residual=False,
+        groups=1,
+        width_per_group=64,
+        replace_stride_with_dilation=None,
+        norm_layer=None,
+        mode=0,
+    ):
+        super(ResNetSigmoid, self).__init__()
+
+        self.mode = mode
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                "or a 3-element tuple, got {}".format(replace_stride_with_dilation)
+            )
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1_sigmoid_norm = ConvSigmoidNorm(
+            3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False, mode=mode
+        )
+        # self.conv1 = nn.Conv2d(
+        #     3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False
+        # )
+        self.bn1 = norm_layer(self.inplanes)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(
+            block,
+            128,
+            layers[1],
+            stride=2,
+            dilate=replace_stride_with_dilation[0],
+        )
+        self.layer3 = self._make_layer(
+            block,
+            256,
+            layers[2],
+            stride=2,
+            dilate=replace_stride_with_dilation[1],
+        )
+        self.layer4 = self._make_layer(
+            block,
+            512,
+            layers[3],
+            stride=2,
+            dilate=replace_stride_with_dilation[2],
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, BottleneckFixed):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(
+            block(
+                self.inplanes,
+                planes,
+                stride,
+                downsample,
+                self.groups,
+                self.base_width,
+                previous_dilation,
+                norm_layer,
+                self.mode,
+            )
+        )
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(
+                block(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                    mode=self.mode,
+                )
+            )
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+        # See note [TorchScript super()]
+        x = self.conv1_sigmoid_norm(x, self.bn1)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+    def forward(self, x):
+        return self._forward_impl(x)
+
+
+def _resnet_sigmoid(arch, block, layers, mode, **kwargs):
+    model = ResNetSigmoid(block, layers, mode=mode, **kwargs)
+    return model
+
+
+def _resnet_fixed(arch, block, layers, mode, **kwargs):
     model = ResNetFixed(block, layers, mode=mode, **kwargs)
     return model
 
@@ -305,7 +431,7 @@ def resnet18_fixed(mode=0, **kwargs):
     Args:
         mode (int): experiment mode
     """
-    return _resnet("resnet18", BasicBlockFixed, [2, 2, 2, 2], mode, **kwargs)
+    return _resnet_fixed("resnet18", BasicBlockFixed, [2, 2, 2, 2], mode, **kwargs)
 
 
 def resnet34_fixed(mode=0, **kwargs):
@@ -315,13 +441,56 @@ def resnet34_fixed(mode=0, **kwargs):
     Args:
         mode (int): experiment mode
     """
-    return _resnet("resnet34", BasicBlockFixed, [3, 4, 6, 3], mode, **kwargs)
+    return _resnet_fixed("resnet34", BasicBlockFixed, [3, 4, 6, 3], mode, **kwargs)
+
+
+def resnet18_sigmoid(mode=0, **kwargs):
+    r"""ResNet-18 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+
+    Args:
+        mode (int): experiment mode
+    """
+    return _resnet_sigmoid(
+        "resnet18", BasicBlockSigmoidGating, [2, 2, 2, 2], mode, **kwargs
+    )
+
+
+def resnet34_sigmoid(mode=0, **kwargs):
+    r"""ResNet-34 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+
+    Args:
+        mode (int): experiment mode
+    """
+    return _resnet_sigmoid(
+        "resnet34", BasicBlockSigmoidGating, [3, 4, 6, 3], mode, **kwargs
+    )
 
 
 if __name__ == "__main__":
     # test defining a model and passing in a tensor
-    model = resnet18_fixed().to("cuda")
-    print(model)
-    # print(model.layer1)
     test_tens = torch.randn(1, 3, 224, 224, device="cuda")
+
+    model = resnet18_fixed().to("cuda")
+    model(test_tens)
+    model = resnet18_sigmoid().to("cuda")
+    model(test_tens)
+    model = resnet34_fixed().to("cuda")
+    model(test_tens)
+    model = resnet34_sigmoid().to("cuda")
+    model(test_tens)
+
+    model = resnet18_fixed(mode=2).to("cuda")
+    model(test_tens)
+    model = resnet18_sigmoid(mode=2).to("cuda")
+    model(test_tens)
+    model = resnet34_fixed(mode=2).to("cuda")
+    model(test_tens)
+    model = resnet34_sigmoid(mode=2).to("cuda")
+    model(test_tens)
+
+    model = resnet18_fixed(mode=3).to("cuda")
+    model(test_tens)
+    model = resnet34_fixed(mode=3).to("cuda")
     model(test_tens)
